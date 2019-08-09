c1ea532cfaf4b6aee58f789b8a5d9e68b32acbaf
Sanxy <yusufadefolahan@gmail.com>
2019-08-09 10:42:54 +0100

    card anim

--- app/src/main/java/com/sanxynet/efullnew/CardPaymentActivity.java
+++ app/src/main/java/com/sanxynet/efullnew/CardPaymentActivity.java
@@ -14,7 +14,7 @@ import java.util.TimerTask;
 
 public class CardPaymentActivity extends AppCompatActivity {
 
-    Handler handler;
+//    Handler handler;
     private TranslateAnimation moveUpwards;
 
     @Override
--- app/src/main/java/com/sanxynet/efullnew/TransactionHistoryActivity.java
+++ app/src/main/java/com/sanxynet/efullnew/TransactionHistoryActivity.java
@@ -1,14 +1,60 @@
 package com.sanxynet.efullnew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionHistoryActivity extends AppCompatActivity {
+
+    private TransactionHistoryAdapter mAdapter;
+//    private List<Transaction> notesList = new ArrayList<>();
+    private CoordinatorLayout coordinatorLayout;
+    private RecyclerView recyclerView;
+    private TextView noNotesView;
+    String db, note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        noNotesView = findViewById(R.id.empty_notes_view);
+
+
+
+//        mAdapter = new TransactionHistoryAdapter(this, note, db);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(mAdapter);
+
+//        toggleEmptyNotes();
+
+//        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this,
+//                recyclerView, new RecyclerTouchListener.ClickListener() {
+//            @Override
+//            public void onClick(View view, final int position) {
+////navigate to single history page
+//                thisTransaction(position);
+//            }
+//
+//            @Override
+//            public void onLongClick(View view, int position) {
+////                showActionsDialog(position);
+//            }
+//        }));
     }
+
+
 }
